636cf7b9116d05c1178b7dfbc765bbd9db10b313
tushar-tayal-t <tushartayal315@gmail.com>
2026-03-15 13:54:23 +0530

    Working on frontend for socket.io integration

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,20 +1,37 @@
 import express from "express";
 import http from "http";
 import { Server } from "socket.io";
+import cors from "cors";
 
 const app = express();
+app.use(express.json());
+app.use(cors());
 const server = http.createServer(app);
 
-const io = new Server(server);
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST"]
+  }
+});
 
-app.use(express.json());
+io.on("connection", (socket)=>{
+  console.log("User connected:", socket.id);
 
-app.get('/', (req, res, next)=>{
-  res.json({
-    message: "This is message",
+  socket.on("send-message", (data)=>{
+    console.log("User send the data:", data);
+    io.emit("receive-message", data);
   });
+
+  // socket.emit("private-message", "Hello, this is just for you!");
+  
+  socket.on("disconnect", ()=>{
+    console.log("user is disconnected");
+  })
 })
 
-app.listen(5000, ()=>{
-  console.log("Server is running at port 3000");
+server.listen(5000, ()=>{
+  console.log("Server is running at port 5000");
 })
+
+
--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,7 +1,39 @@
+"use client";
+import socket from "@/lib/socket";
+import { useEffect, useState } from "react";
+
 export default function Home() {
+  const [message, setMessage] = useState("");
+  const [messages, setMessages] = useState<string[]>([]);
+
+  useEffect(() => {
+    socket.on("receive-message", (data)=>{
+      setMessages((prev) => [...prev, data]);
+    });
+
+    return ()=> {
+      socket.off("receive-message");
+    }
+  }, []);
+
+  const sendMessage = () => {
+    socket.emit("send-message", message);
+    setMessage("");
+  }
+
   return (
     <div>
-      this is div
+      <input
+        value={message}
+        onChange={(e) => setMessage(e.target.value)}
+        placeholder="enter the message"
+      />
+      <button onClick={sendMessage}>Send</button>
+      <div>
+        {messages && messages.map((v, i)=>(
+          <div key={i} >{v}</div>
+        ))}
+      </div>
     </div>
   );
 }
